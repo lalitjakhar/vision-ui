de431990c7df10f317af4414a78aadaa39893974
lalitjakhar <lalit25jakhar@gmail.com>
2023-12-08 17:24:29 +0530

    initial commit

--- src/components/appComponent/dashboard/Home.jsx
+++ src/components/appComponent/dashboard/Home.jsx
@@ -5,10 +5,10 @@ const Home = () => {
   return (
     <>
       <div className="grid grid-cols-6 gap-10">
-        <div className="bg-slate-500 h-12">
+        <div className="bg-inherit">
           <Sidebar />
         </div>
-        <div className="bg-black h-12 col-span-5">
+        <div className="bg-inherit col-span-5">
 
         </div>
       </div>
--- src/components/appComponent/dashboard/Sidebar.jsx
+++ src/components/appComponent/dashboard/Sidebar.jsx
@@ -1,17 +1,139 @@
-import { Button } from "@mui/material";
-import React from "react";
+import Link from "next/link";
 
 const Sidebar = () => {
   return (
-    <>
-      <div>
-        <h1>jhgdfhghd</h1>
-        <Button />
-        <Button />
-        <Button />
-        <Button />
+    <div
+      className="flex-row lg:flex"
+      style={{
+        borderRadius: "16px",
+        background:
+          "linear-gradient(111.84deg, rgba(6, 11, 38, 0.94) 59.3%, rgba(26, 31, 55, 0) 100%)",
+      }}
+    >
+      <div
+        className="flex flex-col lg:h-screen p-2 w-full md:w-72"
+        style={{ margin: "16px 0px 16px 16px" }}
+      >
+        <div className="space-y-3">
+          <div className="flex items-center dashboard_sidebar_heading">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-6 h-6 text-gray-100"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+              />
+            </svg>
+            <h2 className="text-sm font-extrabold text-white">
+              TASK PILOT FRONTEND
+            </h2>
+          </div>
+          <div className="flex-1">
+            <ul className="pt-2 pb-4 space-y-1 text-sm">
+              <li className="rounded-sm">
+                <Link
+                  href="/"
+                  className="flex items-center p-2 space-x-3 rounded-md"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6 text-gray-100"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                    />
+                  </svg>
+                  <span className="text-gray-100">Company</span>
+                </Link>
+              </li>
+              <li className="rounded-sm">
+                <Link
+                  href="mail"
+                  className="flex items-center p-2 space-x-3 rounded-md"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6 text-gray-100"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"
+                    />
+                  </svg>
+                  <span className="text-gray-100">Project</span>
+                </Link>
+              </li>
+              <li className="rounded-sm">
+                <Link
+                  href="/orders"
+                  className="flex items-center p-2 space-x-3 rounded-md"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6 text-gray-100"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
+                    />
+                  </svg>
+                  <span className="text-gray-100">Tag</span>
+                </Link>
+              </li>
+              <li className="rounded-sm">
+                <Link
+                  href="settings"
+                  className="flex items-center p-2 space-x-3 rounded-md"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6 text-gray-100"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+                    />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                    />
+                  </svg>
+                  <span className="text-gray-100">Task</span>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
