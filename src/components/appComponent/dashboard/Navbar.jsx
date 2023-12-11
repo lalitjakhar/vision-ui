83e9c6c3607de5f900bf23a8d251d03a5a958fc5
lalitjakhar <lalit25jakhar@gmail.com>
2023-12-11 16:30:13 +0530

    initial commit

--- src/components/appComponent/dashboard/Navbar.jsx
+++ src/components/appComponent/dashboard/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   AppBar,
   Toolbar,
@@ -6,31 +6,42 @@ import {
   InputBase,
   IconButton,
   Box,
-  Button,
+  MenuItem,
+  Menu,
 } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
+import ReorderSharpIcon from "@mui/icons-material/ReorderSharp";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import LockOpenIcon from "@mui/icons-material/LockOpen";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
 
 const Navbar = () => {
+  const [anchorElUser, setAnchorElUser] = useState(null);
+
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
     <AppBar position="static" className="bg-inherit shadow-none">
-      <Toolbar className="w-full">
+      <Toolbar className="w-full p-0">
+        <ReorderSharpIcon sx={{ display: { xs: "flex", lg: "none" } }} />
         <Typography
           variant="h6"
           sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
         >
           My Navbar
         </Typography>
-        <Box sx={{ display: { xs: "none", sm: "flex" }, gap: 2 }}>
-          <Button color="inherit">Home</Button>
-          <Button color="inherit">About Us</Button>
-        </Box>
         <Box
           sx={{
             position: "relative",
             border: 1,
             borderRadius: 1,
             borderColor: "white",
-            ml: 2,
             p: "2px",
           }}
         >
@@ -42,6 +53,42 @@ const Navbar = () => {
             placeholder="Type here..."
           />
         </Box>
+        <IconButton
+          sx={{ ml: 2, color: "white" }}
+          onClick={handleOpenUserMenu}
+        >
+          <AccountCircleIcon />
+        </IconButton>
+        <Menu
+          sx={{ mt: "50px", width:"600px" }}
+          id="menu-appbar"
+          anchorEl={anchorElUser}
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          open={Boolean(anchorElUser)}
+          onClose={handleCloseUserMenu}
+        >
+          <Typography>dgsdsdsgfdfs</Typography>
+          <MenuItem onClick={handleCloseUserMenu}>
+            <LockOpenIcon sx={{ mr: 2 }} />
+            Sign In
+          </MenuItem>
+          <MenuItem onClick={handleCloseUserMenu}>
+            <PersonAddIcon sx={{ mr: 2 }} />
+            Sign Up
+          </MenuItem>
+          <MenuItem onClick={handleCloseUserMenu}>
+            <AccountCircleIcon sx={{ mr: 2 }} />
+            Profile
+          </MenuItem>
+        </Menu>
       </Toolbar>
     </AppBar>
   );
